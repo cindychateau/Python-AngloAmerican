fb2f633c7036e8ebac0f9de7c77075e6f98f109d
Cynthia Castillo <ca.castillo@gmail.com>
2023-12-04 18:16:26 -0600

    Contador

---
+++ 4.- Flask/contador/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, redirect, session
+
+app = Flask(__name__)
+app.secret_key = "llave super secreta"
+
+#Guardar en session['contador'] la cantidad de veces que se ha ingresado al sitio
+@app.route('/')
+def index():
+    if 'contador' in session:
+        session['contador'] += 1
+    else:
+        session['contador'] = 1
+    return render_template('index.html')
+
+@app.route('/destroy_session')
+def destroy():
+    session.clear()
+    return redirect('/')
+
+@app.route('/mas-dos')
+def masdos():
+    session['contador'] += 1
+    return redirect('/')
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
