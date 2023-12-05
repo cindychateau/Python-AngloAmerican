b923539dd284f65ff2cc7e63048cae5bf03044d9
Cynthia Castillo <ca.castillo@gmail.com>
2023-12-04 18:16:10 -0600

    Sesiones e Inputs ocultos

--- 4.- Flask/formulario/server.py
+++ 4.- Flask/formulario/server.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+
+app.secret_key = "llave secreta." #establece una clave/llave secreta para dar más seguridad a nuestras cookies, que es donde se guarda la sesión
 
 @app.route("/")
 def index():
@@ -10,10 +12,15 @@ def index():
 def enviar():
     #request.form = {
     #   "nombre": "Elena de Troya"    
-    #   "email": "elena@codingdojo.com"    
+    #   "email": "elena@codingdojo.com",
+    #   "tipo_usuario": "normal"   
     #}
     print(request.form)
     #return "Info recibida."+request.form['nombre']
+
+    session['usuario'] = request.form['nombre']
+    session['correo'] = request.form['email']
+
     return redirect("/exito")
 
 @app.route("/exito")
