6349d029b12d23ee083d615564943d858c7a97d9
Cynthia Castillo <ca.castillo@gmail.com>
2023-12-18 18:00:22 -0600

    Login+Registro y Nueva receta

---
+++ 4.- Flask/recetas/flask_app/controllers/recipes_controller.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect, session, request, flash
+from flask_app import app
+
+#MODELOS
+from flask_app.models.users import User
+from flask_app.models.recipes import Recipe
+
+#RUTAS
+
+#1 ruta para desplegar el formulario (Debemos de revisar que se haya iniciado sesión)
+#HTML con el formulario y todos los inputs a guardar
+@app.route('/recipes/new')
+def recipes_new():
+    #Verificar que el usuario haya iniciado sesión
+    if 'user_id' not in session:
+        flash('Favor de iniciar sesión', 'not_in_session')
+        return redirect('/')
+    
+    return render_template('new.html')
+
+
+#2 ruta para guardar (Debemos de revisar que se haya iniciado sesión)
+#Validar la información
+#Guarda la info
+@app.route('/recipes/create', methods=['POST'])
+def recipes_create():
+    #Verificar que el usuario haya iniciado sesión
+    if 'user_id' not in session:
+        flash('Favor de iniciar sesión', 'not_in_session')
+        return redirect('/')
+    
+    #Verificar que el formulario esté llenado correctamente
+    if not Recipe.validate_recipe(request.form):
+        return redirect('/recipes/new')
+    
+    #Guardar la informacion
+    Recipe.save(request.form)
+    return redirect('/dashboard')
